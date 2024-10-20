1f0a64ef0e416904b86a76d8cd4f1cc8835af8d9
name db incognita <wubecodes12345@gmail.com>
2024-10-20 17:43:43 +0530

    changes

--- functions/app.js
+++ functions/app.js
@@ -14,36 +14,42 @@ const app = express();
 const port = process.env.PORT || 3031;
 const databaseURL = process.env.DATABASE_URL;
 
-app.use(cors({
-    origin: process.env.ORIGIN,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], 
-    credentials: true,
-}))
+// app.use(cors({
+//     origin: process.env.ORIGIN,
+//     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], 
+//     credentials: true,
+// }))
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const router = Router();
-app.use(router);
-app.use("/.netlify/functions/app", router);
-router.get("/", (req, res) => {
-    res.send("App is running..");
-});
-router.post("/api/auth/signup", signup);
-router.post("/api/auth/signin", signin);
-router.post("/api/auth/refresh", refresh);
-router.get("/api/addsingle", addSingle);
-router.post("/api/data", data);
-router.get("/api/user", user);
+// const router = Router();
+// app.use(router);
+app.use("/.netlify/functions/api", (res, req) => res.json({mssg:"smthing cooking....."}));
+// router.get("/", (req, res) => {
+//     res.send("App is running..");
+// });
+// router.post("/api/auth/signup", signup);
+// router.post("/api/auth/signin", signin);
+// router.post("/api/auth/refresh", refresh);
+// router.get("/api/addsingle", addSingle);
+// router.post("/api/data", data);
+// router.get("/api/user", user);
 
 
-const server = app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-})
+// const server = app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// })
 
 mongoose
     .connect(databaseURL)
     .then(() => console.log("DB connected!"));
 
-export default serverless(app);
+
+const handler =  serverless(app);
+
+module.exports.handler = async(e, c) => {
+    const res = await handler(e, c);
+    return res;
+}
